07c8b41c42f7e025a6fcb948f96bed4249156fbe
THE GRID <34685278+Predvkill@users.noreply.github.com>
2019-02-19 23:29:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ client.on('ready', () => {
                msg.edit('**:arrows_counterclockwise: Shaddix Restarting...**');
             },2000);
         });
-        console.log(`${message.author.tag} [ ${message.author.id} ] Jarvis Has Restarted Successfully.`);
+        console.log(`${message.author.tag} [ ${message.author.id} ] Shaddix Has Restarted Successfully.`);
         console.log(`Restarting..`);
         setTimeout(() => {
             client.destroy();
